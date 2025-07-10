0fe446c0a381cd408ec1f8a828b50d407955ef7b
josethz00 <jtsoares17@hotmail.com>
2025-07-10 12:29:42 -0300

    chore: change remote bucket credentials

--- config/object_storage.py
+++ config/object_storage.py
@@ -1,12 +1,10 @@
 from config import env
 
-S3_DELTA_PATH = f"s3://{env.MINIO_BUCKET}/test-table"
-
 object_storage_options = {
-    "AWS_ACCESS_KEY_ID": env.AWS_ACCESS_KEY_ID,
-    "AWS_SECRET_ACCESS_KEY": env.AWS_SECRET_ACCESS_KEY,
-    "AWS_REGION": env.AWS_REGION,
-    "AWS_ALLOW_HTTP": "true",
+    "ACCESS_KEY_ID": env.AWS_ACCESS_KEY_ID,
+    "SECRET_ACCESS_KEY": env.AWS_SECRET_ACCESS_KEY,
+    "REGION": env.AWS_REGION,
+    "ALLOW_HTTP": "true",
     "endpoint_url": env.MINIO_HOST,
-    "DELTA_LAKE_URL": f"s3://{env.MINIO_BUCKET}/"
+    "DELTA_LAKE_URL": f"s3://{env.MINIO_BUCKET}/",
 }
